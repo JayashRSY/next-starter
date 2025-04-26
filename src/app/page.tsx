2a99479bb7fc40f2a2308ea3b1aaec9f130fe449
Jayash Raj Singh Yadav <65907404+JayashRSY@users.noreply.github.com>
2025-04-26 20:17:47 +0530

    login redirect fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
-  router.push("/dashboard");
-  return
-  <>
-    Home
-  </>
+  
+  useEffect(() => {
+    router.push("/dashboard");
+  }, [router]);
+  
+  return (
+    <>
+      Home
+    </>
+  );
 }
